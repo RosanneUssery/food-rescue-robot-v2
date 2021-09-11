147d2206ded1474a2d3481026b6958c2768248bd
Rosanne Ussery <rkussery@gmail.com>
2021-09-11 09:54:14 -0600

    added routes to front end, axios, tailwind

--- food-robot-v2-front/src/main.js
+++ food-robot-v2-front/src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
+import './main.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   components: { App },
   template: '<App/>'
 })
