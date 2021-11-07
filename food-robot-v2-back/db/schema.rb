34fbd5f26627b3a98698f08d6aa4a920bb5ba591
Ruthie Rabinovitch <ruthie.rabinovitch@ibotta.com>
2021-11-07 01:24:23 -0700

    update users table migration to include uuid

--- food-robot-v2-back/db/schema.rb
+++ food-robot-v2-back/db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_075055) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
