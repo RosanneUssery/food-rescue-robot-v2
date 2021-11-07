d1aac148664aaffb95dcb8fe211c35bb241ad3b6
Ruthie Rabinovitch <ruthie.rabinovitch@ibotta.com>
2021-11-07 01:44:42 -0700

    set up access_level enum values

--- food-robot-v2-back/app/models/user.rb
+++ food-robot-v2-back/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum access_level: [:staff, :volunteer]
 end
