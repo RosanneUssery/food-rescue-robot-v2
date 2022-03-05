3b3cb9b3e185ef008c83f9d3f81be71dfaeee01f
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:34:14 -0700

    Remove directive

--- food-robot-v2-front/src/components/NavBar.vue
+++ food-robot-v2-front/src/components/NavBar.vue
@@ -69,26 +69,6 @@
   import Vue, { VNode } from 'vue';
   import { Component } from 'vue-property-decorator';
 
-  interface HTMLElement {
-      clickOutsideEvent: () => void;
-  }
-
-    Vue.directive('click-outside', {
-        bind: function (el: any, binding: DirectiveBinding, vnode: VNode) {
-            el.clickOutsideEvent = function (event: Event) {
-                // here I check that click was outside the el and his childrens
-                if (!(el == event.target || el.contains(event.target))) {
-                    // and if it did, call method provided in attribute value
-                    vnode.context[binding.expression](event);
-                }
-            };
-            document.body.addEventListener('click', el.clickOutsideEvent)
-        },
-        unbind: function (el: any) {
-            document.body.removeEventListener('click', el.clickOutsideEvent)
-        },
-    });
-
   @Component
   export default class extends Vue {
 
