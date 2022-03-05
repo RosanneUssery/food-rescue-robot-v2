bdcac75fb75b0a187e7d627b619b78eb13b3ca58
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 14:22:48 -0700

    Add layout component

---
+++ food-robot-v2-front/src/components/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="LayoutComponent max-w-7xl mx-auto mt-8 ">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+
+  @Component
+  export default class extends Vue {
+
+
+  }
+</script>
+
+<style>
+
+
+</style>
