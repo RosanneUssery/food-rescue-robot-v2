ebf72c6ae1214303088732a7f94e1dd696c420de
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:34:45 -0700

    Create LoggedIn view

---
+++ food-robot-v2-front/src/views/LoggedIn.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <NavBar />
+        <Layout>
+            <router-view />
+        </Layout> 
+    </div>
+      
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+  import NavBar from '@/components/NavBar.vue';
+  import Layout from '@/components/Layout.vue';
+
+  @Component({
+    components: {
+      NavBar,
+      Layout
+    }
+  })
+  export default class extends Vue {
+
+
+  }
+
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+
+</style>
