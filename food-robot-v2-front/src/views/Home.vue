63aac13f7aecfff390d76c2c1a494d040e3fc7a9
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 10:38:00 -0700

    Setup Vue CLI project

---
+++ food-robot-v2-front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="this is my vue component"/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+  import HelloWorld from '../components/HelloWorld.vue'
+
+  @Component({
+    components: {
+      HelloWorld
+    }
+  })
+  export default class extends Vue {
+  }
+</script>
