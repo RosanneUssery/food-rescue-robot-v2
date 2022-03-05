e5c68ca537f0f20b00bd0c9ec76a17eb69bf5bcc
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:34:32 -0700

    Add route for login

--- food-robot-v2-front/src/router/index.ts
+++ food-robot-v2-front/src/router/index.ts
@@ -1,20 +1,45 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import LoggedIn from '../views/LoggedIn.vue';
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'LoggedIn',
+    component: LoggedIn,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+
+      },
+      {
+        path: '/takeshifts',
+        name: 'TakeShifts',
+        component: () => import(/* webpackChunkName: "take-shifts" */ '../views/TakeShifts.vue')
+      },
+      {
+        path: '/yourshifts',
+        name: 'YourShifts',
+        component: () => import(/* webpackChunkName: "your-shifts" */ '../views/YourShifts.vue')
+      },
+      {
+        path: '/volunteers',
+        name: 'Volunteers',
+        component: () => import(/* webpackChunkName: "volunteers" */ '../views/Volunteers.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+
 ]
 
 const router = new VueRouter({
