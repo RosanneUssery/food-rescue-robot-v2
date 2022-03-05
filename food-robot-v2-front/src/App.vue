8d821eb0e681e82693975b1f7d7be9f9b6820baa
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:48:23 -0700

    Remove unused components

--- food-robot-v2-front/src/App.vue
+++ food-robot-v2-front/src/App.vue
@@ -1,34 +1,26 @@
 <template>
   <div id="app">
-      <router-view/>
+    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
+
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
-  import NavBar from '@/components/NavBar.vue';
-  import Layout from '@/components/Layout.vue';
 
-  @Component({
-    components: {
-      NavBar,
-      Layout
-    }
-  })
+  @Component
   export default class extends Vue {
 
-
-  }
+}
 
 </script>
 
 <style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
 </style>
