636185b19b065f3b57a4ff7a0bc8a991c94f2fc7
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:48:36 -0700

    Remove style tag

--- food-robot-v2-front/src/components/Layout.vue
+++ food-robot-v2-front/src/components/Layout.vue
@@ -5,17 +5,11 @@
 </template>
 
 <script lang="ts">
+
   import Vue from 'vue';
   import { Component } from 'vue-property-decorator';
 
   @Component
-  export default class extends Vue {
+  export default class extends Vue {}
 
-
-  }
 </script>
-
-<style>
-
-
-</style>
