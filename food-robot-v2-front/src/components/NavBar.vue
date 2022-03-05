baa658427acd755d0933081a4c58cc7ba0d25e7e
Edward Stoner <edwardlstoner@gmail.com>
2022-03-05 16:48:50 -0700

    Add dynamic imports

--- food-robot-v2-front/src/components/NavBar.vue
+++ food-robot-v2-front/src/components/NavBar.vue
@@ -29,25 +29,22 @@
                         </div>
                     </div>
                 </div>
-                      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-
-                        <!-- Profile dropdown -->
-                        <div class="ml-3 relative">
-                            <div>
-                                <button type="button" class="bg-gray-800 flex text-sm rounded-full" id="user-menu-button" aria-expanded="false" aria-haspopup="true" @click="handleProfileMenuClick">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="../assets/saleck.jpg" alt="">
-                                </button>
-                            </div>
-                            <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" v-if="ProfileMenuOpen">
-                                <!-- Active: "bg-gray-100", Not Active: "" -->
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                            </div>
+                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <div class="ml-3 relative">
+                        <div>
+                            <button type="button" class="bg-gray-800 flex text-sm rounded-full" id="user-menu-button" aria-expanded="false" aria-haspopup="true" @click="handleProfileMenuClick">
+                            <span class="sr-only">Open user menu</span>
+                            <img class="h-8 w-8 rounded-full" src="../assets/saleck.jpg" alt="">
+                            </button>
+                        </div>
+                        <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" v-if="ProfileMenuOpen">
+                            <!-- Active: "bg-gray-100", Not Active: "" -->
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
                         </div>
                     </div>
+                </div>
             </div>
             <div class="sm:hidden" id="mobile-menu" v-if="MobileMenuOpen">
                 <div class="px-2 pt-2 pb-3 space-y-1">
@@ -66,31 +63,23 @@
 </template>
 
 <script lang="ts">
-  import Vue, { VNode } from 'vue';
-  import { Component } from 'vue-property-decorator';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 
-  @Component
-  export default class extends Vue {
+@Component
+export default class extends Vue {
 
-      MobileMenuOpen = false;
-      ProfileMenuOpen = false;
+    MobileMenuOpen = false;
+    ProfileMenuOpen = false;
 
-      handleMenuClick() {
-          this.MobileMenuOpen = !this.MobileMenuOpen;
-      }
+    handleMenuClick() {
+        this.MobileMenuOpen = !this.MobileMenuOpen;
+    }
 
-      handleProfileMenuClick() {
-          this.ProfileMenuOpen = !this.ProfileMenuOpen;
-      }
-
-
-  }
-</script>
-
-<style>
-
-  .NavbarComponent {
+    handleProfileMenuClick() {
+        this.ProfileMenuOpen = !this.ProfileMenuOpen;
+    }
 
 }
 
-</style>
+</script>
